babfff13ad6c5f8b07274447a178a09fd5ec910d
mikeremerscheid <mikeremerscheid@gmail.com>
2021-03-01 23:03:27 -0500

    Added some more basics

---
+++ arrays.py
@@ -0,0 +1,20 @@
+cars = ["Ford", "Volvo", "BMW"]
+# x = cars[0]
+cars[0] = "Toyota"
+# x = cars[0]
+# print(x)
+# print(len(x))
+cars.append("Honda")
+cars.pop(1)
+
+# cars.clear()
+
+
+for x in cars:
+    print(x)
+    
+cars.sort()
+
+print x in cars:
+    print(x)    
+
---
+++ ifelseloopswhileloops.py
@@ -0,0 +1,68 @@
+# a = 33
+# b = 200
+# if b > a:
+#     print("b is greater than a")
+    
+# a = 33
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+    
+    
+# a = 200
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+# else:
+#     print("a is greater than b")
+    
+# a = 200
+# b = 33
+# if a>b: print("a is greater than b")
+
+# a = 2
+# b = 330
+# print("A") if a > b else print("B")
+
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a ==b else print ("B")
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits: 
+#     print(x)
+    
+# for x in "banana":
+#     print(x)
+    
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits: 
+#     print(x)
+#     if x == "banana":
+#         break
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits: 
+#     if x == "banana":
+#         break
+#     print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     if x == "banana":
+#         continue
+#     print(x)
+    
+# for x in range(6):
+#     print(x)
+    
+# for x in range(2,6):
+#     print(x)
+    
+# for x in range(2, 30, 3):
+#     print(x)
+    
